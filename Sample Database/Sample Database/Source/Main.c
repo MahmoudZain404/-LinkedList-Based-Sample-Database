17039186005f47c4e7284556b90064a1f28a9db8
Mahmoud Zain <111197800+SHAD0W4O4@users.noreply.github.com>
2023-08-15 02:29:08 +0300

    Add files via upload

---
+++ Sample Database/Sample Database/Source/Main.c
@@ -0,0 +1,35 @@
+/* Sample Database */
+
+/*
+ * Main.c
+ *
+ * Created: 8/11/2023 11:15:48 PM
+ *  Author: Mahmoud Zain Salah
+ */
+
+
+
+
+#include "source.c"
+
+int main()
+{
+
+
+	
+	Add_Student();
+	Add_Student();
+	Add_Student();
+	Add_Student();
+
+	Delete_student();
+
+	Add_Student();
+
+
+	PrintList();
+
+	
+	Search_ID();
+
+}
