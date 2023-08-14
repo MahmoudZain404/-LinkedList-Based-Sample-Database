923abd198491e5c6df8fa6fd13b34112261b840c
Mahmoud Zain <111197800+SHAD0W4O4@users.noreply.github.com>
2023-08-15 02:22:12 +0300

    Update base.h

--- Sample Database/Sample Database/Header/base.h
+++ Sample Database/Sample Database/Header/base.h
@@ -34,9 +34,9 @@ void Add_Student(void);
 
 int Sort_list(struct student** head);
 
-void Delete_student();
+void Delete_student(void);
 
-struct student* Search_ID();
+struct student* Search_ID(void);
 
 struct student* get_middle(struct student* start, struct student* end);
 
